1d219973af42cae6a5f9e4a8c6a517814ea168e0
Sheyne Anderson <sanderson@novacoast.com>
2019-07-04 02:05:35 -0700

    implement letrec

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,16 @@ use std::ops;
 use std::rc::Rc;
 use std::str::CharIndices;
 
+#[macro_use]
+extern crate lazy_static;
+
 type Env<'a> = HashMap<&'a str, Value<'a>>;
 
 #[derive(Debug, Clone, PartialEq)]
-enum Expr<'a> {
+pub enum Expr<'a> {
     Add(Box<Expr<'a>>, Box<Expr<'a>>),
     Sub(Box<Expr<'a>>, Box<Expr<'a>>),
-    Let(&'a str, Box<Expr<'a>>, Box<Expr<'a>>),
+    If0(Box<Expr<'a>>, Box<Expr<'a>>, Box<Expr<'a>>),
     Lambda(&'a str, Box<Expr<'a>>),
     Call(Box<Expr<'a>>, Box<Expr<'a>>),
     Id(&'a str),
@@ -17,9 +20,9 @@ enum Expr<'a> {
 }
 
 #[derive(Debug, Clone)]
-enum Value<'a> {
+pub enum Value<'a> {
     Num(f64),
-    Clos(&'a str, Rc<Expr<'a>>),
+    Clos(&'a str, Rc<Env<'a>>, Rc<Expr<'a>>),
 }
 
 impl<'a> PartialEq for Value<'a> {
@@ -62,18 +65,28 @@ impl<'a> ops::Sub<Value<'a>> for Value<'a> {
     }
 }
 
-fn eval<'a, 'b>(e: &Expr<'a>, env: &'b Env<'a>) -> Value<'a> {
+pub fn eval<'a, 'b>(e: &Expr<'a>, env: &'b Env<'a>) -> Value<'a> {
     match e {
         Expr::Lit(x) => Value::Num(*x),
         Expr::Add(left, right) => eval(left, env) + eval(right, env),
         Expr::Sub(left, right) => eval(left, env) - eval(right, env),
-        Expr::Let(id, value, expr) => eval(expr, &add_to_env(id, eval(value, env), env)),
+        Expr::If0(cond, t, f) => {
+            if let Value::Num(n) = eval(cond, env) {
+                if n == 0.0 {
+                    eval(t, env)
+                } else {
+                    eval(f, env)
+                }
+            } else {
+                panic!("")
+            }
+        }
         Expr::Id(id) => get_id(id, &env),
-        Expr::Lambda(id, expr) => Value::Clos(id, Rc::new(*expr.clone())),
+        Expr::Lambda(id, expr) => Value::Clos(id, Rc::new(env.clone()), Rc::new(*expr.clone())),
         Expr::Call(func, arg) => {
             let argv = eval(arg, env);
-            if let Value::Clos(argn, body) = eval(func, env) {
-                eval(&body, &add_to_env(argn, argv, env))
+            if let Value::Clos(argn, lex_env, body) = eval(func, env) {
+                eval(&body, &add_to_env(argn, argv, &*lex_env))
             } else {
                 panic!("cannot call non-closure")
             }
@@ -92,10 +105,6 @@ fn get_id<'a, 'b>(id: &str, env: &'b Env<'a>) -> Value<'a> {
         None => panic!("{} not declared", id),
         Some(a) => a.clone(),
     }
-}
-
-fn lit<'a>(x: f64) -> Box<Expr<'a>> {
-    Box::new(Expr::Lit(x))
 }
 
 #[derive(Debug, PartialEq)]
@@ -228,14 +237,39 @@ fn sexp_to_expr<'a>(sexp: &SExp<'a>) -> Expr<'a> {
                 Box::new(sexp_to_expr(&elements[1])),
                 Box::new(sexp_to_expr(&elements[2])),
             ),
-            SExp::Leaf("let") => Expr::Let(
-                match &elements[1] {
-                    SExp::Leaf(x) => x,
-                    _ => panic!("not a name to bind"),
-                },
+            SExp::Leaf("if0") => Expr::If0(
+                Box::new(sexp_to_expr(&elements[1])),
                 Box::new(sexp_to_expr(&elements[2])),
                 Box::new(sexp_to_expr(&elements[3])),
             ),
+            SExp::Leaf("let") => Expr::Call(
+                Box::new(Expr::Lambda(
+                    match &elements[1] {
+                        SExp::Leaf(x) => x,
+                        _ => panic!("not a name to bind"),
+                    },
+                    Box::new(sexp_to_expr(&elements[3])),
+                )),
+                Box::new(sexp_to_expr(&elements[2])),
+            ),
+            SExp::Leaf("letrec") => {
+                lazy_static! {
+                    static ref Y : Expr<'static> = parse("(lambda f ((lambda x (x x)) (lambda x (f (lambda y ((x x) y))))))");
+                }
+                let n = match &elements[1] {
+                    SExp::Leaf(x) => x,
+                    _ => panic!("not a name to bind"),
+                };
+                let call_y = Expr::Call(
+                    Box::new(Y.clone()),
+                    Box::new(Expr::Lambda(n, Box::new(sexp_to_expr(&elements[2])))),
+                );
+
+                Expr::Call(
+                    Box::new(Expr::Lambda(n, Box::new(sexp_to_expr(&elements[3])))),
+                    Box::new(call_y),
+                )
+            }
             SExp::Leaf("lambda") => Expr::Lambda(
                 match &elements[1] {
                     SExp::Leaf(x) => x,
@@ -251,23 +285,26 @@ fn sexp_to_expr<'a>(sexp: &SExp<'a>) -> Expr<'a> {
     }
 }
 
-fn parse<'a>(program: &'a str) -> Expr<'a> {
+pub fn parse<'a>(program: &'a str) -> Expr<'a> {
     let mut tokens = tokenize(program);
     let exp = parse_sexp(&mut tokens);
     sexp_to_expr(&exp)
 }
 
 fn main() {
-    let y = Expr::Add(lit(17.0), Box::new(Expr::Sub(lit(7.5), lit(7.0))));
+    let y = parse("(lambda f ((lambda x (x x)) (lambda x (f (lambda y ((x x) y))))))");
 
-    println!("Match value: {:?}", eval(&y, &HashMap::new()));
+    println!("{:?}", y);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
+    fn lit<'a>(x: f64) -> Box<Expr<'a>> {
+        Box::new(Expr::Lit(x))
+    }
+
     #[test]
     fn test_add() {
         assert_eq!(
@@ -289,37 +326,25 @@ mod tests {
 
     #[test]
     fn test_lookup() {
-        assert_eq!(
-            eval(&Expr::Id("x"), &HashMap::unit("x", Value::Num(14.0))),
-            Value::Num(14.0)
-        )
+        let env = HashMap::unit("x", Value::Num(14.0));
+
+        assert_eq!(eval(&Expr::Id("x"), &env), Value::Num(14.0))
     }
 
     #[test]
     #[should_panic(expected = "y not declared")]
     fn test_lookup_not_found() {
-        assert_eq!(
-            eval(&Expr::Id("y"), &HashMap::unit("x", Value::Num(14.0))),
-            Value::Num(14.0)
-        )
-    }
+        let env = HashMap::unit("x", Value::Num(14.0));
 
-    #[test]
-    fn test_let() {
-        assert_eq!(
-            eval(
-                &Expr::Let("x", lit(17.0), Box::new(Expr::Id("x"))),
-                &HashMap::new()
-            ),
-            Value::Num(17.0)
-        )
+        assert_eq!(eval(&Expr::Id("y"), &env), Value::Num(14.0))
     }
 
     #[test]
     fn test_lambda() {
         let func = Expr::Lambda("x", Box::new(Expr::Add(lit(17.0), Box::new(Expr::Id("x")))));
-        if let Value::Clos(arg, body) = eval(&func, &HashMap::new()) {
+        if let Value::Clos(arg, env, body) = eval(&func, &HashMap::new()) {
             assert_eq!(arg, "x");
+            assert_eq!(*env, HashMap::new());
             assert_eq!(*body, Expr::Add(lit(17.0), Box::new(Expr::Id("x"))));
         } else {
             panic!("didn't eval to closure")
@@ -409,13 +434,6 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_let() {
-        assert_eq!(
-            parse("(let x 1 x)"),
-            Expr::Let("x", lit(1.), Box::new(Expr::Id("x")))
-        )
-    }
-    #[test]
     fn test_parse_clos() {
         assert_eq!(
             parse("(lambda x (+ 1 x))"),
@@ -454,5 +472,48 @@ mod tests {
     fn test_eval_complexer() {
         let prog = parse("((lambda x (+ x 1)) 3)");
         assert_eq!(eval(&prog, &HashMap::new()), (Value::Num(4.)))
+    }
+
+    #[test]
+    fn test_if_0() {
+        assert!(eval(&parse("(if0 0 1 2)"), &HashMap::new()) == Value::Num(1.));
+        assert!(eval(&parse("(if0 1 1 2)"), &HashMap::new()) == Value::Num(2.));
+    }
+
+    #[test]
+    fn test_nesting() {
+        let nested = parse("(((lambda y (lambda x (+ x y))) 1) 2)");
+        assert_eq!(eval(&nested, &HashMap::new()), Value::Num(3.0))
+    }
+
+    #[test]
+    fn test_y_combin() {
+        let y = parse("(lambda f ((lambda x (x x)) (lambda x (f (lambda y ((x x) y))))))");
+
+        let sum = parse(
+            "(lambda f (lambda x 
+                (if0 x 
+                    0
+                    (+ x (f (- x 1))))))",
+        );
+
+        let call_with_3 = Expr::Call(Box::new(Expr::Call(Box::new(y), Box::new(sum))), lit(3.0));
+
+        assert_eq!(eval(&call_with_3, &HashMap::new()), Value::Num(6.0));
+    }
+
+    #[test]
+    fn test_letrec() {
+        let sum = parse(
+            "(letrec f (lambda x 
+                (if0 x 
+                    0
+                    (+ x (f (- x 1))))) (f 5))",
+        );
+
+        assert_eq!(
+            eval(&sum, &HashMap::new()),
+            Value::Num(1. + 2. + 3. + 4. + 5.)
+        );
     }
 }
