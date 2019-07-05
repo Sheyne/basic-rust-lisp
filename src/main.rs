6c4617ea4d7ff46d06895094ba1fb0772ddbde17
Sheyne Anderson <sanderson@novacoast.com>
2019-07-05 16:10:45 -0700

    Start work on typing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use atomic_counter::{AtomicCounter, RelaxedCounter};
 use im::hashmap::HashMap;
 use std::cmp::Ordering;
 use std::ops;
@@ -8,6 +9,8 @@ use std::str::CharIndices;
 extern crate lazy_static;
 
 type Env<'a> = HashMap<&'a str, Value<'a>>;
+type TEnv<'a> = HashMap<&'a str, Type>;
+type TypeConstraints = HashMap<Sym, Type>;
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum ExprKind<'a> {
@@ -42,17 +45,39 @@ pub struct Expr<'a> {
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Value<'a> {
-    Num(f64),
+    Double(f64),
     Bool(bool),
     String(String),
     Clos(&'a str, Rc<Env<'a>>, Rc<Expr<'a>>),
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
+pub struct Sym(usize);
+
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum Type {
+    Var(Sym),
+    Poly,
+    Double,
+    Bool,
+    String,
+    Clos(Box<Type>, Box<Type>),
+}
+
+impl Sym {
+    fn new() -> Sym {
+        lazy_static! {
+            static ref C: RelaxedCounter = RelaxedCounter::new(0);
+        }
+        Sym(C.inc())
+    }
+}
+
 impl<'a> PartialOrd for Value<'a> {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         match self {
-            Value::Num(l) => match other {
-                Value::Num(r) => l.partial_cmp(r),
+            Value::Double(l) => match other {
+                Value::Double(r) => l.partial_cmp(r),
                 _ => None,
             },
             Value::Bool(l) => match other {
@@ -73,8 +98,8 @@ impl<'a> ops::Add<Value<'a>> for Value<'a> {
 
     fn add(self, rhs: Self) -> Self {
         match self {
-            Value::Num(l) => match rhs {
-                Value::Num(r) => Value::Num(l + r),
+            Value::Double(l) => match rhs {
+                Value::Double(r) => Value::Double(l + r),
                 _ => panic!("can only add nums"),
             },
             _ => panic!("can only add nums"),
@@ -86,8 +111,8 @@ impl<'a> ops::Sub<Value<'a>> for Value<'a> {
 
     fn sub(self, rhs: Value) -> Value {
         match self {
-            Value::Num(l) => match rhs {
-                Value::Num(r) => Value::Num(l - r),
+            Value::Double(l) => match rhs {
+                Value::Double(r) => Value::Double(l - r),
                 _ => panic!("can only sub nums"),
             },
             _ => panic!("can only sub nums"),
@@ -99,8 +124,8 @@ impl<'a> ops::Div<Value<'a>> for Value<'a> {
 
     fn div(self, rhs: Value) -> Value {
         match self {
-            Value::Num(l) => match rhs {
-                Value::Num(r) => Value::Num(l / r),
+            Value::Double(l) => match rhs {
+                Value::Double(r) => Value::Double(l / r),
                 _ => panic!("can only sub nums"),
             },
             _ => panic!("can only sub nums"),
@@ -112,8 +137,8 @@ impl<'a> ops::Mul<Value<'a>> for Value<'a> {
 
     fn mul(self, rhs: Value) -> Value {
         match self {
-            Value::Num(l) => match rhs {
-                Value::Num(r) => Value::Num(l * r),
+            Value::Double(l) => match rhs {
+                Value::Double(r) => Value::Double(l * r),
                 _ => panic!("can only sub nums"),
             },
             _ => panic!("can only sub nums"),
@@ -121,10 +146,150 @@ impl<'a> ops::Mul<Value<'a>> for Value<'a> {
     }
 }
 
+fn assert_type(source: Type, expected: Type, constraints: TypeConstraints) -> TypeConstraints {
+    if source != expected {
+        match &source {
+            Type::Var(s) => {
+                let mut new = constraints.clone();
+                if let Some(old) = new.get(s) {
+                    panic!("already has {:?}: {:?} -> {:?}", s, old, expected);
+                };
+
+                new.insert(*s, expected);
+                new
+            }
+            Type::Clos(arg_s, res_s) => match &expected {
+                Type::Var(_) => assert_type(expected, source, constraints),
+                Type::Clos(arg_e, res_e) => {
+                    let constraints = assert_type(*arg_s.clone(), *arg_e.clone(), constraints);
+                    assert_type(*res_s.clone(), *res_e.clone(), constraints)
+                }
+                _ => panic!("Unresolvable types {:?} != {:?}", source, expected),
+            },
+            _ => match &expected {
+                Type::Var(_) => assert_type(expected, source, constraints),
+                _ => panic!("Unresolvable types {:?} != {:?}", source, expected),
+            },
+        }
+    } else {
+        constraints
+    }
+}
+
+fn lookup(constraints: TypeConstraints, t: Type) -> (TypeConstraints, Type) {
+    match t {
+        Type::Var(s) => {
+            let t = if let Some(t) = constraints.get(&s) {
+                t.clone()
+            } else {
+                t
+            };
+            (constraints, t)
+        }
+        Type::Clos(a, b) => {
+            let (constraints, a) = lookup(constraints, *a);
+            let (constraints, b) = lookup(constraints, *b);
+            (constraints, Type::Clos(Box::new(a), Box::new(b)))
+        }
+        _ => (constraints, t),
+    }
+}
+
+pub fn typecheck<'a, 'b>(
+    e: &Expr<'a>,
+    env: &'b TEnv<'a>,
+    constraints: TypeConstraints,
+) -> (TypeConstraints, Type) {
+    let constrain_binary = |left: &Expr<'a>,
+                            right: &Expr<'a>,
+                            t_in: Type,
+                            t_out: Type,
+                            constraints: TypeConstraints|
+     -> (TypeConstraints, Type) {
+        let (constraints, left_t) = typecheck(left, env, constraints);
+        let constraints = assert_type(left_t, t_in.clone(), constraints);
+        let (constraints, right_t) = typecheck(right, env, constraints);
+        let constraints = assert_type(right_t, t_in, constraints);
+        lookup(constraints, t_out)
+    };
+
+    match &e.kind {
+        ExprKind::Add(left, right) => {
+            constrain_binary(left, right, Type::Double, Type::Double, constraints)
+        }
+        ExprKind::Sub(left, right) => {
+            constrain_binary(left, right, Type::Double, Type::Double, constraints)
+        }
+        ExprKind::Mul(left, right) => {
+            constrain_binary(left, right, Type::Double, Type::Double, constraints)
+        }
+        ExprKind::Div(left, right) => {
+            constrain_binary(left, right, Type::Double, Type::Double, constraints)
+        }
+        ExprKind::Lt(left, right) => {
+            constrain_binary(left, right, Type::Double, Type::Bool, constraints)
+        }
+        ExprKind::Gt(left, right) => {
+            constrain_binary(left, right, Type::Double, Type::Bool, constraints)
+        }
+        ExprKind::Eq(left, right) => {
+            constrain_binary(left, right, Type::Double, Type::Bool, constraints)
+        }
+        ExprKind::Not(left) => {
+            let (constraints, t) = typecheck(left, env, constraints);
+            lookup(assert_type(t, Type::Bool, constraints), Type::Bool)
+        }
+        ExprKind::Lit(x) => match x {
+            LiteralKind::Double(_) => (constraints, Type::Double),
+            LiteralKind::Bool(_) => (constraints, Type::Bool),
+            LiteralKind::String(_) => (constraints, Type::String),
+        },
+        ExprKind::Concat(left, right) => {
+            constrain_binary(left, right, Type::String, Type::String, constraints)
+        }
+        ExprKind::If(cond, t, f) => {
+            let (constraints, cond_t) = typecheck(cond, env, constraints);
+            let constraints = assert_type(cond_t, Type::Bool, constraints);
+            let (constraints, t_t) = typecheck(t, env, constraints);
+            let (constraints, f_t) = typecheck(f, env, constraints);
+            let constraints = assert_type(t_t.clone(), f_t, constraints);
+            lookup(constraints, t_t)
+        }
+        ExprKind::Lambda(id, expr) => {
+            let arg_sym = Sym::new();
+            let arg = Type::Var(arg_sym);
+            let mut new_env = env.clone();
+            new_env.insert(id, arg.clone());
+            let (constraints, body_t) = typecheck(expr, &new_env, constraints);
+
+            if let Some(x) = constraints.get(&arg_sym) {
+                println!("Binding {:?} = {:?}", &arg_sym, x);
+            } else {
+                println!("Binding {:?} = None", &arg_sym);
+            }
+
+            lookup(constraints, Type::Clos(Box::new(arg), Box::new(body_t)))
+        }
+        ExprKind::Call(func, arg) => {
+            let (constraints, func_t) = typecheck(func, env, constraints);
+            let (constraints, arg_t) = typecheck(arg, env, constraints);
+            let res_t = Type::Var(Sym::new());
+            let constraints = assert_type(
+                func_t,
+                Type::Clos(Box::new(arg_t), Box::new(res_t.clone())),
+                constraints,
+            );
+            lookup(constraints, res_t)
+        }
+        ExprKind::Id(x) => {
+            (constraints, env.get(x).unwrap().clone())}
+    }
+}
+
 pub fn eval<'a, 'b>(e: &Expr<'a>, env: &'b Env<'a>) -> Value<'a> {
     match &e.kind {
         ExprKind::Lit(x) => match x {
-            LiteralKind::Double(d) => Value::Num(*d),
+            LiteralKind::Double(d) => Value::Double(*d),
             LiteralKind::Bool(b) => Value::Bool(*b),
             LiteralKind::String(b) => Value::String(String::from(*b)),
         },
@@ -585,7 +750,7 @@ mod tests {
     fn test_add() {
         assert_eq!(
             eval(&add(double_v(17.0), double_v(3.2)), &HashMap::new()),
-            Value::Num(20.2)
+            Value::Double(20.2)
         );
     }
 
@@ -596,7 +761,7 @@ mod tests {
                 &add(double_v(17.0), sub(double_v(7.5), double_v(7.0))),
                 &HashMap::new()
             ),
-            Value::Num(17.5)
+            Value::Double(17.5)
         );
     }
 
@@ -622,17 +787,17 @@ mod tests {
 
     #[test]
     fn test_lookup() {
-        let env = HashMap::unit("x", Value::Num(14.0));
+        let env = HashMap::unit("x", Value::Double(14.0));
 
-        assert_eq!(eval(&id("x"), &env), Value::Num(14.0))
+        assert_eq!(eval(&id("x"), &env), Value::Double(14.0))
     }
 
     #[test]
     #[should_panic(expected = "y not declared")]
     fn test_lookup_not_found() {
-        let env = HashMap::unit("x", Value::Num(14.0));
+        let env = HashMap::unit("x", Value::Double(14.0));
 
-        assert_eq!(eval(&id("y"), &env), Value::Num(14.0))
+        assert_eq!(eval(&id("y"), &env), Value::Double(14.0))
     }
 
     #[test]
@@ -651,19 +816,19 @@ mod tests {
     fn test_math() {
         assert_eq!(
             eval(&add(double_v(3.), double_v(5.)), &HashMap::new()),
-            Value::Num(3. + 5.)
+            Value::Double(3. + 5.)
         );
         assert_eq!(
             eval(&sub(double_v(3.), double_v(5.)), &HashMap::new()),
-            Value::Num(3. - 5.)
+            Value::Double(3. - 5.)
         );
         assert_eq!(
             eval(&mul(double_v(3.), double_v(5.)), &HashMap::new()),
-            Value::Num(3. * 5.)
+            Value::Double(3. * 5.)
         );
         assert_eq!(
             eval(&div(double_v(3.), double_v(5.)), &HashMap::new()),
-            Value::Num(3. / 5.)
+            Value::Double(3. / 5.)
         );
     }
 
@@ -716,7 +881,7 @@ mod tests {
         let func = lambda("x", add(double_v(17.0), id("x")));
         let arg = double_v(11.0);
 
-        assert_eq!(eval(&call(func, arg), &HashMap::new()), Value::Num(28.0))
+        assert_eq!(eval(&call(func, arg), &HashMap::new()), Value::Double(28.0))
     }
 
     #[test]
@@ -956,31 +1121,49 @@ mod tests {
     #[test]
     fn test_eval_simple() {
         let prog = parse("(+ 1 1)");
-        assert_eq!(eval(&prog, &HashMap::new()), (Value::Num(2.)))
+
+        assert_eq!(
+            typecheck(&prog, &HashMap::new(), HashMap::new()),
+            (HashMap::new(), Type::Double)
+        );
+
+        assert_eq!(eval(&prog, &HashMap::new()), (Value::Double(2.)))
     }
 
     #[test]
     fn test_eval_complex() {
         let prog = parse("(let x (lambda x (+ x 1)) (x 4))");
-        assert_eq!(eval(&prog, &HashMap::new()), Value::Num(5.))
+
+        assert_eq!(
+            typecheck(&prog, &HashMap::new(), HashMap::new()),
+            (HashMap::new(), Type::Double)
+        );
+
+        assert_eq!(eval(&prog, &HashMap::new()), Value::Double(5.))
     }
 
     #[test]
     fn test_eval_complexer() {
         let prog = parse("((lambda x (+ x 1)) 3)");
-        assert_eq!(eval(&prog, &HashMap::new()), (Value::Num(4.)))
+
+        assert_eq!(
+            typecheck(&prog, &HashMap::new(), HashMap::new()),
+            (HashMap::new(), Type::Double)
+        );
+
+        assert_eq!(eval(&prog, &HashMap::new()), (Value::Double(4.)))
     }
 
     #[test]
     fn test_if() {
-        assert!(eval(&parse("(if (< 0 1) 1 2)"), &HashMap::new()) == Value::Num(1.));
-        assert!(eval(&parse("(if (< 1 0) 1 2)"), &HashMap::new()) == Value::Num(2.));
+        assert!(eval(&parse("(if (< 0 1) 1 2)"), &HashMap::new()) == Value::Double(1.));
+        assert!(eval(&parse("(if (< 1 0) 1 2)"), &HashMap::new()) == Value::Double(2.));
     }
 
     #[test]
     fn test_nesting() {
         let nested = parse("(((lambda y (lambda x (+ x y))) 1) 2)");
-        assert_eq!(eval(&nested, &HashMap::new()), Value::Num(3.0))
+        assert_eq!(eval(&nested, &HashMap::new()), Value::Double(3.0))
     }
 
     #[test]
@@ -996,7 +1179,7 @@ mod tests {
 
         let call_with_3 = call(call(y, sum), double_v(3.0));
 
-        assert_eq!(eval(&call_with_3, &HashMap::new()), Value::Num(6.0));
+        assert_eq!(eval(&call_with_3, &HashMap::new()), Value::Double(6.0));
     }
 
     #[test]
@@ -1032,8 +1215,13 @@ mod tests {
         );
 
         assert_eq!(
+            typecheck(&sum, &HashMap::new(), HashMap::new()),
+            (HashMap::new(), Type::Double)
+        );
+
+        assert_eq!(
             eval(&sum, &HashMap::new()),
-            Value::Num(1. + 2. + 3. + 4. + 5.)
+            Value::Double(1. + 2. + 3. + 4. + 5.)
         );
     }
 }
